bd0439d075523ae19c1bd6f340c63663905eea16
Ruth Salvador <ruth.abby.sz@gmail.com>
2017-04-10 17:50:36 -0500

    DOM proyects

---
+++ assets/js/app.js
@@ -0,0 +1,28 @@
+window.addEventListener('load', function(){
+
+  function addProyect(num){
+    for (var i = 1; i <= num; i++) {
+      var proyectsContainer = document.getElementById("proyects-container");
+    /*  var box = document.createElement("div");
+      box.setAttribute("class","div-box");
+      principal.appendChild(box);*/
+
+      var figure = document.createElement("figure");
+      figure.setAttribute("class","figure-proyect");
+      proyectsContainer.appendChild(figure);
+
+      var image = document.createElement("img");
+      image.setAttribute("src","assets/img/img-"+ i + ".jpg");
+      //image.setAttribute("class","image-coder");
+      figure.appendChild(image);
+
+      var title = document.createElement("figcaption");
+      title.setAttribute("class","name-proyect");
+      figure.appendChild(title);
+
+      title.appendChild(document.createTextNode("NOMBRE DEL PROYECTO"));
+    }
+  }
+  addProyect(12);
+
+})
