d11620b44fdf30437f049181f87f85c041bde1ff
Ruth Salvador <ruth.abby.sz@gmail.com>
2017-04-11 15:43:02 -0500

    final projects

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,17 +1,17 @@
 window.addEventListener('load', function(){
 
-var projects = ["TAYMAN GROUP", "NALIA ORGANICS", "BRASS COQ","SUNGENRE","LIVING BOTANICS",
-"FOAM KING","VOLITION","INK","GILKON","OUTS APPLES","SHERWOOD","PLATINUM"];
+var projects = ["TAYMAN GROUP", "NALIA ORGANICS", "BRASS COQ","SUNGENRE", "LIVING BOTANICS",
+"FOAM KING", "VOLITION", "ink", "GILKON", "OUTS APPLES", "SHERWOOD", "PLATINUM"];
 
   function addProject(array){
-    for (var i = 0; i < array.length; i++) {
+    projects.forEach(function(e,i){
       var projectsContainer = document.getElementById("projects-container");
 
       var figure = document.createElement("figure");
       figure.setAttribute("class","figure-project");
       projectsContainer.appendChild(figure);
 
-      var anImg =  document.createElement("a");
+      var anImg = document.createElement("a");
       anImg.setAttribute("href", "#modal-"+(i+1));
       anImg.setAttribute("class","img-link");
       figure.appendChild(anImg);
@@ -23,18 +23,18 @@ var projects = ["TAYMAN GROUP", "NALIA ORGANICS", "BRASS COQ","SUNGENRE","LIVING
       anImg.appendChild(image);
 
       var name = document.createElement("figcaption");
-      name.setAttribute("class","name-project");
+      name.setAttribute("class","name-project text-uppercase");
       figure.appendChild(name);
 
       name.appendChild(document.createTextNode(array[i]));
-    }
+    });
   }
   addProject(projects);
 
   /*********************Agregando los modales*********************/
 
   function addModals(array){
-    for (var i = 0; i < array.length; i++) {
+    projects.forEach(function(e,i){
       var projectsBig = document.getElementById("projects-big");
 
       var figureModal = document.createElement("figure");
@@ -42,18 +42,23 @@ var projects = ["TAYMAN GROUP", "NALIA ORGANICS", "BRASS COQ","SUNGENRE","LIVING
       figureModal.setAttribute("id", "modal-" + (i+1));
       projectsBig.appendChild(figureModal);
 
+      var divModal = document.createElement("div");
+      divModal.setAttribute("class", "modal-div");
+      figureModal.appendChild(divModal);
+
       var imgModal = document.createElement("img");
       imgModal.setAttribute("src","assets/img/img-"+ (i+1) + ".jpg");
-      imgModal.setAttribute("width","600");
-      figureModal.appendChild(imgModal);
+      imgModal.setAttribute("class", "modal-image");
+      imgModal.setAttribute("width","728");
+      divModal.appendChild(imgModal);
 
       var cerrarModal = document.createElement("a");
       cerrarModal.setAttribute("class", "close-modal");
       cerrarModal.setAttribute("href", "#projects-container");
-      figureModal.appendChild(cerrarModal);
+      divModal.appendChild(cerrarModal);
 
       cerrarModal.appendChild(document.createTextNode("x"));
-    }
+    });
   }
   addModals(projects);
 
